3ebb980e475b3ee080b3d199416e68786b5b81c4
aguirrehdezma <aguirrehdezma@gmail.com>
2025-04-11 08:16:25 -0700

    first commit

---
+++ app/src/main/java/mx/edu/itesca/crud_firebase/Tarea.kt
@@ -0,0 +1,15 @@
+package mx.edu.itesca.crud_firebase
+
+data class Tarea(
+    var id: String = "",
+    var titulo: String = "",
+    var descripcion: String = ""
+)
+{
+    fun toMap(): Map<String, String> {
+        return mapOf(
+            "titulo" to titulo,
+            "descripcion" to descripcion
+        )
+    }
+}
